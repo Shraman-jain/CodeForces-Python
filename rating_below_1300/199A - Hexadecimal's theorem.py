ccc06ef31c461cf7a6148eb8620b3cb6564c6574
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2023-08-13 01:01:47 +0530

    initial

--- rating_below_1300/199A - Hexadecimal's theorem.py
+++ rating_below_1300/199A - Hexadecimal's theorem.py
@@ -1,2 +1,3 @@
-#lol this was very easy and funny I wasted a lot of time thinking it
+#lol this was very easy and funny I wasted a lot of time thinking it.
+# I am really feeling Dumb
 print(0,0,int(input()))
